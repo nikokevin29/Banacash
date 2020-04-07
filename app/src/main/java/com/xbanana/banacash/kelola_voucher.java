831aec0acf091a528977e1f92d8ae42e79341ac1
Renzvin <kskkrenzz@gmail.com>
2020-04-07 18:53:57 +0700

    fixing bug

--- app/src/main/java/com/xbanana/banacash/kelola_voucher.java
+++ app/src/main/java/com/xbanana/banacash/kelola_voucher.java
@@ -7,16 +7,28 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.xbanana.banacash.API.APInterface;
 import com.xbanana.banacash.API.ApiClient;
 import com.xbanana.banacash.DAO.VoucherDAO;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.SQLOutput;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.Response;
 
 public class kelola_voucher extends AppCompatActivity {
     ListView listView;
@@ -33,26 +45,57 @@ public class kelola_voucher extends AppCompatActivity {
         setVoucherList();
     }
     public void setVoucherList(){
-        APInterface apiService = ApiClient.getClient().create(APInterface.class);
-        Call<List<VoucherDAO>> voucherDAOCall = apiService.showAllVoucher();
+        String url = "http://banacash.luxinoire.com/api/showVoucher";
+        RequestQueue queue = Volley.newRequestQueue(kelola_voucher.this);
 
-        voucherDAOCall.enqueue(new Callback<List<VoucherDAO>>() {
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
-            public void onResponse(Call<List<VoucherDAO>> call, Response<List<VoucherDAO>> response) {
-                if(response.isSuccessful()){
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    for(int i = 0; i<jsonArray.length(); i++) {
+                        JSONObject obj = jsonArray.getJSONObject(i);
+                            VoucherDAO voucher = new VoucherDAO(obj.getInt("id"),obj.getString("kode"),obj.getInt("diskon"));
+                            voucherList.add(voucher);
+                    }
                     Toast.makeText(kelola_voucher.this, "Show Voucher", Toast.LENGTH_SHORT).show();
-                    voucherList.addAll(response.body());
-                    System.out.println(response.body().get(0).getKode());
-                }else {
-                    Toast.makeText(kelola_voucher.this, "UnSuccessful Get Voucher",Toast.LENGTH_SHORT).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
             }
-
+        }, new Response.ErrorListener() {
             @Override
-            public void onFailure(Call<List<VoucherDAO>> call, Throwable t) {
-                Toast.makeText(kelola_voucher.this, "Kesalahan Jaringan",Toast.LENGTH_SHORT).show();
+            public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(kelola_voucher.this,"Connection Error" ,Toast.LENGTH_SHORT).show();
             }
         });
-
+        queue.add(getRequest);
+//        APInterface apiService = ApiClient.getClient().create(APInterface.class);
+//        Call<List<VoucherDAO>> voucherDAOCall = apiService.showAllVoucher();
+//
+//        voucherDAOCall.enqueue(new Callback<List<VoucherDAO>>() {
+//            @Override
+//            public void onResponse(Call<List<VoucherDAO>> call, Response<List<VoucherDAO>> response) {
+//                if(response.isSuccessful()){
+//                    Toast.makeText(kelola_voucher.this, "Show Voucher", Toast.LENGTH_SHORT).show();
+//                    voucherList.addAll(response.body());
+//                    System.out.println(response.body().get(0).getKode());
+//                }else {
+//                    Toast.makeText(kelola_voucher.this, "UnSuccessful Get Voucher",Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<VoucherDAO>> call, Throwable t) {
+//                if (t instanceof IOException) {
+//                    Toast.makeText(kelola_voucher.this, "this is an actual network failure :( inform the user and possibly retry", Toast.LENGTH_SHORT).show();
+//                    // logging probably not necessary
+//                }
+//                else {
+//                    Toast.makeText(kelola_voucher.this, "conversion issue! big problems :(", Toast.LENGTH_SHORT).show();
+//                    // todo log to some central bug tracking service
+//                }
+//            }
+//        });
     }
 }
