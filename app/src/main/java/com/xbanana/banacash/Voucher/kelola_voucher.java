91b8aa90dcf4eb2000e0fe3be9e56481698ffe02
Renzvin <kskkrenzz@gmail.com>
2020-04-08 12:15:39 +0700

    fixing bug

--- app/src/main/java/com/xbanana/banacash/Voucher/kelola_voucher.java
+++ app/src/main/java/com/xbanana/banacash/Voucher/kelola_voucher.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -42,13 +43,8 @@ public class kelola_voucher extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
-
     private Button AddVoucher;
-    private EditText Kode,Persen;
 
-    private PopupWindow popupWindow;
-    private LayoutInflater layoutInflater;
-    private ConstraintLayout constraints;
     @SuppressLint("ClickableViewAccessibility")
 
     @Override
@@ -56,15 +52,11 @@ public class kelola_voucher extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kelola_voucher);
 
-
-
-
-
-        constraints = (ConstraintLayout) findViewById(R.id.constraint);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view_voucher);
         voucherList = new ArrayList<>();
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
+
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(layoutManager);
@@ -77,61 +69,14 @@ public class kelola_voucher extends AppCompatActivity {
         AddVoucher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                layoutInflater =(LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
-                ViewGroup container = (ViewGroup) layoutInflater.inflate(R.layout.pop_add_voucher,null);
-                popupWindow = new PopupWindow(container,800,500,true);
-                popupWindow.showAtLocation(constraints,Gravity.CENTER,0,0);
-                //View popupView = layoutInflater.inflate(R.layout.pop_add_voucher, null);
-
-//                Button SubmitAddVoucher = findViewById(R.id.btnSumbitAdd);
-//                SubmitAddVoucher.setOnClickListener(new View.OnClickListener() {
-//                    @Override
-//                    public void onClick(View v) {
-//                        if(Kode.getText().length() == 0 ||Persen.getText().length() == 0){
-//                            Toast.makeText(kelola_voucher.this, "Field Cant be Empty", Toast.LENGTH_SHORT).show();
-//                        }else{
-//                            addData();
-//                            popupWindow.dismiss();
-//                        }
-//                    }
-//                });
-//                container.setOnTouchListener(new View.OnTouchListener() {
-//                    @Override
-//                    public boolean onTouch(View v, MotionEvent event) {
-//                        return true;
-//                    }
-//                });
+                openDialog();
             }
         });
 
     }
-    public void addData(){
-        APInterface apiService = ApiClient.getClient().create(APInterface.class);
-        Call<List<VoucherDAO>> voucherDAO = apiService.showAllVoucher();
-
-        voucherDAO.enqueue(new Callback<List<VoucherDAO>>() {
-            @Override
-            public void onResponse(Call<List<VoucherDAO>> call, Response<List<VoucherDAO>> response) {
-                APInterface apiService = ApiClient.getClient().create(APInterface.class);
-                Call<VoucherDAO> callDAO = apiService.createVoucher(Kode.getText().toString(),Persen.getText().toString());
-                callDAO.enqueue(new Callback<VoucherDAO>() {
-                    @Override
-                    public void onResponse(Call<VoucherDAO> call, Response<VoucherDAO> response) {
-                        Toast.makeText(kelola_voucher.this, "Success Adding Data", Toast.LENGTH_SHORT).show();
-                    }
-                    @Override
-                    public void onFailure(Call<VoucherDAO> call, Throwable t) {
-                        Toast.makeText(kelola_voucher.this, "Failed Enqueue", Toast.LENGTH_SHORT).show();
-                        System.out.println("ERR_ADD"+t.getMessage());
-                    }
-                });
-            }
-            @Override
-            public void onFailure(Call<List<VoucherDAO>> call, Throwable t) {
-                System.out.println("gagal");
-                Toast.makeText(kelola_voucher.this, "Failed create Data", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void openDialog(){
+        pop_up_voucher dialog = new pop_up_voucher();
+        dialog.show(getSupportFragmentManager(),"Creae Voucher");
     }
     public void showData(){
         APInterface apiService = ApiClient.getClient().create(APInterface.class);
---
+++ app/src/main/java/com/xbanana/banacash/Voucher/pop_up_voucher.java
@@ -0,0 +1,86 @@
+package com.xbanana.banacash.Voucher;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.xbanana.banacash.API.APInterface;
+import com.xbanana.banacash.API.ApiClient;
+import com.xbanana.banacash.DAO.VoucherDAO;
+import com.xbanana.banacash.R;
+
+import java.security.PrivateKey;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class pop_up_voucher extends AppCompatDialogFragment {
+    private EditText kode,diskon;
+    private Button submit;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog( Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.pop_add_voucher,null);
+        kode = (EditText)view.findViewById(R.id.etAddKode);
+        diskon = (EditText)view.findViewById(R.id.etAddPersentase);
+
+        builder.setView(view).setTitle("Add New Voucher").setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        }).setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                addData();
+            }
+        });
+        return builder.create();
+    }
+    public void addData() {
+        APInterface apiService = ApiClient.getClient().create(APInterface.class);
+        Call<List<VoucherDAO>> voucherDAO = apiService.showAllVoucher();
+
+        voucherDAO.enqueue(new Callback<List<VoucherDAO>>() {
+            @Override
+            public void onResponse(Call<List<VoucherDAO>> call, Response<List<VoucherDAO>> response) {
+                APInterface apiService = ApiClient.getClient().create(APInterface.class);
+                Call<VoucherDAO> callDAO = apiService.createVoucher(kode.getText().toString(), diskon.getText().toString());
+                callDAO.enqueue(new Callback<VoucherDAO>() {
+                    @Override
+                    public void onResponse(Call<VoucherDAO> call, Response<VoucherDAO> response) {
+                        Toast.makeText(getContext(), "Success Adding Data", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<VoucherDAO> call, Throwable t) {
+                        Toast.makeText(getContext(), "Failed Enqueue", Toast.LENGTH_SHORT).show();
+                        System.out.println("ERR_ADD" + t.getMessage());
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(Call<List<VoucherDAO>> call, Throwable t) {
+                System.out.println("gagal");
+                Toast.makeText(getActivity().getApplicationContext(), "Failed create Data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
